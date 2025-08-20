f850248ceaf862190d65e09e42dfe1595a58a892
ConsorcioBRT <ti@brtgo.com.br>
2025-08-20 17:24:57 -0300

    ajustado a questão da Data da Operação de madrugada D - 1

--- app/api/recarga/route.ts
+++ app/api/recarga/route.ts
@@ -39,6 +39,15 @@ export async function GET() {
 }
 
 // Aqui será o POST
+
+// Aqui vai ser para a Data da Operação ser sempre o dia anterior quando for 00h até 4:59h
+function ajustarDtaOpe(dtaIni: Date, dtaOpep: Date) {
+  const hora = dtaIni.getHours();
+  if (hora >= 0 && hora < 5) {
+    dtaOpep.setDate(dtaOpep.getDate() - 1);
+  }
+  return dtaOpep;
+}
 export async function POST(request: Request) {
   try {
     const { UndId, VclId, CrrId, CrrCnc, SocIni, OdoFin, UsrIdAlt, EmpId } =
@@ -67,9 +76,18 @@ export async function POST(request: Request) {
 
     // Aqui se a última recarga terminar com erro, usar ele como orgigem
     let rcgIdOrgNovo = 0;
-    if (ultimaRecarga && ultimaRecarga.SttRcgId === 7) {
-      rcgIdOrgNovo = ultimaRecarga.RcgId;
+    let dtaOpeNovo = new Date();
+    if (ultimaRecarga) {
+      if (ultimaRecarga.SttRcgId === 7 && ultimaRecarga.FlhId === 1) {
+        rcgIdOrgNovo = ultimaRecarga.RcgIdOrg || ultimaRecarga.RcgId; // aqui irá salvar o RcgIdOrg atual com o RcgIdOrg anterior
+        dtaOpeNovo = ultimaRecarga.DtaOpe;
+      }
     }
+
+    // Aqui vai ajustar o horário das 00h até 5h
+    const agora = new Date();
+    dtaOpeNovo = ajustarDtaOpe(agora, dtaOpeNovo);
+
     // Aqui será a criação da Nova Recarga
     const novaRecarga = await prisma.rcg.create({
       data: {
@@ -77,8 +95,8 @@ export async function POST(request: Request) {
         VclId: Number(VclId),
         CrrId: Number(CrrId),
         CrrCnc: Number(CrrCnc),
-        DtaIni: new Date(),
-        DtaOpe: new Date(),
+        DtaIni: agora,
+        DtaOpe: dtaOpeNovo,
         SocIni: Number(SocIni),
         OdoIni: ultimaRecarga?.OdoFin || null, // se não houver anterior, fica null
         OdoFin: OdoFin !== undefined ? Number(OdoFin) : null,
@@ -163,31 +181,25 @@ export async function PUT(request: Request) {
       dadosAtualizados.OdoFin = Number(OdoFin);
     }
 
-    if (FlhId === 1) {
-      // finalizada com erro
-      dadosAtualizados.FlhId = 1;
-      dadosAtualizados.SttRcgId = 7;
-    } else if (recargaExistente.SttRcgId === 5) {
-      // finalizada normalmente
-      dadosAtualizados.FlhId = 0;
-      dadosAtualizados.SttRcgId = 6;
-    }
-
     if (FlhDsc && !recargaExistente.FlhDsc) {
       dadosAtualizados.FlhDsc = FlhDsc;
     }
 
     if (forcarSttRcgId6) {
-      dadosAtualizados.SttRcgId = 6;
+      dadosAtualizados.SttRcgId = 6; // aqui vai atualiazr normalmente sem erro
       dadosAtualizados.FlhId = FlhId || 0;
     } else if (FlhId === 1) {
       // finalizada com erro
       dadosAtualizados.FlhId = 1;
       dadosAtualizados.SttRcgId = 7;
+      dadosAtualizados.RcgIdOrg =
+        recargaExistente.RcgIdOrg || recargaExistente.RcgId; // aqui vai salvar o RcgId no RcgIdOrg
     } else if (recargaExistente.SttRcgId === 5) {
       // finalizada normalmente
       dadosAtualizados.FlhId = 0;
       dadosAtualizados.SttRcgId = 6;
+    } else if (!recargaExistente.RcgIdOrg) {
+      dadosAtualizados.RcgIdOrg = recargaExistente.RcgId;
     }
 
     const recargaAtualizada = await prisma.rcg.update({

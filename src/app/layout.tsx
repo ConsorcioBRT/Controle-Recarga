4215dc91de27139aec30ac97c9190ba3783ac51a
ConsorcioBRT <ti@brtgo.com.br>
2025-09-04 09:43:00 -0300

    adicionando insights no projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./global.css";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
         <Toaster position="bottom-center" />
+        <SpeedInsights />
       </body>
     </html>
   );

2e857276a191acfe77cd9e776f2ca707b2eebb2f
ConsorcioBRT <ti@brtgo.com.br>
2025-09-24 17:05:48 -0300

    ajustando a foto 3.1

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ignora warnings de ESLint no build
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push("ssh2", "ssh2-sftp-client");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
